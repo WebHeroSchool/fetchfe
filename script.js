961493c0a755b9e290df65d6ef9d3332c66b9047
ElenaSindi <sindielena@icloud.com>
2020-10-18 20:28:51 +0300

    добавила script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+
+fetch('https://api.github.com/users/mojombo')
+	.then(res => res.json())
+	.then(json => {
+		console.log(json.name)
+		console.log(json.bio)
+		console.log(json.html_url)
+		console.log(json.avatar_url)
+	
+	//document.write(json.login)
+
+	let link = document.querySelector('.link');
+	let img = document.querySelector('.img');
+	let bio = document.querySelector('.bio');
+	link.innerHTML = json.name;
+	link.addEventListener('click', () => {
+		window.location = json.html_url;
+	})
+	if (json.bio !== null) {
+		bio.innerHTML = json.bio;
+	} else {
+		bio.innerHTML += "Информация о пользователе не доступна."
+	}
+	
+	img.src = json.avatar_url;
+
+	})
