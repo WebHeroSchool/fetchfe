f7e80a03b1e96394b7ee46c1eb2985e4d9959c85
ElenaSindi <sindielena@icloud.com>
2020-10-22 19:16:09 +0300

    изменила script.js и style.css

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ setTimeout(() => {
     preloaderEl.classList.remove('visible');
     text.classList.add('visible');
    	text.classList.remove('hidden');
-  }, 2000);
+  }, 4000);
 let date = new Date;
  let getDate = new Promise((resolve, reject) => {
  	setTimeout(() => date ? resolve(document.getElementById("time").innerHTML = date) : reject(error), 4000)
@@ -13,13 +13,20 @@ let date = new Date;
 //getDate
 	//.then(result => document.getElementById("time").innerHTML = result)
 
-fetch('https://api.github.com/users/mojombo')
-	.then(res => res.json())
-	.then(json => {
-		console.log(json.name)
-		console.log(json.bio)
-		console.log(json.html_url)
-		console.log(json.avatar_url)
+let url = 'https://api.github.com/users/mojombo';
+let getUrl = new Promise((resolve, reject) => {
+	resolve(url);
+	reject('Ссылка не найдена');	
+})
+
+Promise.all([getUrl, getDate])
+		.then(([url, date]) => fetch(url))
+		.then(res => res.json())
+		.then(json => {
+			console.log(json.name)
+			console.log(json.bio)
+			console.log(json.html_url)
+			console.log(json.avatar_url)
 
 	let link = document.querySelector('.link');
 	let img = document.querySelector('.img');
@@ -27,48 +34,12 @@ fetch('https://api.github.com/users/mojombo')
 	link.innerHTML = json.name;
 	link.addEventListener('click', () => {
 		window.location = json.html_url;
-	})
-	let getBio = new Promise((resolve, reject) => {
-		if (json.bio !== null) {
-			resolve(bio.innerHTML = json.bio);
-	} else {
-			reject(bio.innerHTML += "Информация о пользователе не доступна.");
-	}	
-	})
-	
-	Promise.all([getDate, getBio])
-		.then(result => result)
-	
-	img.src = json.avatar_url;
-
-	}) 
-	
-// было до этого
-/*
-fetch('https://api.github.com/users/mojombo')
-	.then(res => res.json())
-	.then(json => {
-		console.log(json.name)
-		console.log(json.bio)
-		console.log(json.html_url)
-		console.log(json.avatar_url)
-	
-	
-
-	let link = document.querySelector('.link');
-	let img = document.querySelector('.img');
-	let bio = document.querySelector('.bio');
-	link.innerHTML = json.name;
-	link.addEventListener('click', () => {
-		window.location = json.html_url;
-	})
+	});
 	if (json.bio !== null) {
 		bio.innerHTML = json.bio;
-	} else {
+		} else {
 		bio.innerHTML += "Информация о пользователе не доступна."
-	}
-	
+		}
 	img.src = json.avatar_url;
+	});	
 
-	})
-*/
